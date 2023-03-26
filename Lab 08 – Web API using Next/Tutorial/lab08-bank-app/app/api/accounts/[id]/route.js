c92f43f47817971215d219c29dc3181b1aecc50e
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-03-26 16:32:18 +0300

    Update route.js

--- Lab 08 – Web API using Next/Tutorial/lab08-bank-app/app/api/accounts/[id]/route.js
+++ Lab 08 – Web API using Next/Tutorial/lab08-bank-app/app/api/accounts/[id]/route.js
@@ -3,7 +3,7 @@ const repo = new AccountRepo()
 
 export async function GET(request, { params }) {
     const { id } = params
-    const account = await repo.getAccountById(id)
+    const account = await repo.getAccount(id)
     return Response.json(account, { status: 200 })
 
 }
