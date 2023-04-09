c087fae01546f86b492086195c130c136c247b13
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-04-09 16:54:03 +0300

    updated Account

--- Lab 9– Web Pages using Next.js and React/Tutorial/lab08-bank-app/app/accounts/AccountsTable.js
+++ Lab 9– Web Pages using Next.js and React/Tutorial/lab08-bank-app/app/accounts/AccountsTable.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import Account from './Account'
+import styles from '../page.module.css'
 
 export default function AccountsTable({ initialAccounts }) {
     return (
         <div>
-            {
-                initialAccounts.map(account => <p>{account.accountNo}</p>)
-            }
+            <table id="accounts" className={styles.table}>
+                <tr>
+                    <th>Account No</th>
+                    <th>Account Type</th>
+                    <th>Balance</th>
+                    <th>Action</th>
+                </tr>
+                {initialAccounts.map(account => <Account account={account}> </Account>)}
+            </table>
         </div>
     )
 }
