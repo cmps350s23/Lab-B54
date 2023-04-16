ce503a661a9743dcd77d31a418040fd6f727e49d
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-04-16 15:24:20 +0300

    add account completed

--- Lab 10– Pages Part 2/Tutorial/lab08-bank-app/app/accounts/add/page.js
+++ Lab 10– Pages Part 2/Tutorial/lab08-bank-app/app/accounts/add/page.js
@@ -10,12 +10,22 @@ export default function AddAccount() {
         newAccount[e.target.name] = e.target.value
         setAccount(newAccount)
     }
+    async function handleSubmit(e) {
+        e.preventDefault()
+        const data = await fetch('/api/accounts', {
+            method: 'POST',
+            body: JSON.stringify(account),
+            headers: { 'Content-Type': 'application/json' }
+        })
+        const newAccount = await data.json()
+        console.log(newAccount);
+    }
 
     return (
         <>
             {JSON.stringify(account)}
             <h3>Add Account</h3>
-            <form id="account-form" className={styles.form}>
+            <form id="account-form" className={styles.form} onSubmit={handleSubmit}>
                 <label htmlFor="acctType" className={styles.label}>Account Type</label>
                 <select name="acctType" id="acctType" required onChange={handleChange}>
                     <option></option>
