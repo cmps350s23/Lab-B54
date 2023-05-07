1bbca4ce657ebc2c825f7c2ab91b913375e50ff7
Abdulahi Hassen <abdala.baaji@gmail.com>
2023-05-07 19:31:34 +0300

    Update route.js

--- Lab 12 – Data Management using Prisma Part 2/lab12-bank-app/app/api/accounts/report/route.js
+++ Lab 12 – Data Management using Prisma Part 2/lab12-bank-app/app/api/accounts/report/route.js
@@ -14,7 +14,9 @@ export async function POST(request) {
     // console.log(query);
     // const report = await repo.getAvgBalance()
     // const report = await repo.getMinMaxBalance()
-    const report = await repo.getTop3Accounts()
+    // const report = await repo.getTop3Accounts()
+    // const report = await repo.getTransSum()
+    const report = await repo.getOwnerReport('ckockkdifg2fpt')
     return Response.json(report)
 
 }
